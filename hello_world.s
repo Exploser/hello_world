3c11e2357372e3805c006196728a99679a646c66
Exploser <exploserbusiness@gmail.com>
2024-03-08 16:49:55 -0500

    Initial Commit

---
+++ hello_world.s
@@ -0,0 +1,23 @@
+.global _start
+
+.intel_syntax noprefix
+
+_start:
+	//sys_write
+	mov rax, 1
+	mov rdi, 1
+	lea esi, [hello_world]
+	mov rdx, 14
+	syscall
+
+	//sys_exit
+	mov rax, 60
+	mov rdi, 69
+	syscall
+
+	mov rax, 60
+	mov rdi, 69
+	syscall
+
+hello_world:
+	.asciz "Hello, World!\n"
